d111e5992a006ecf0e320dc14472b810c7db7fab
TbOMu4 <artemtumoshenko@gmail.com>
2019-06-02 00:18:43 +0300

    hw3-3

--- js/js.module-3/js/index.js
+++ js/js.module-3/js/index.js
@@ -2,27 +2,20 @@
 
 const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
 
-
 const isLoginValid = function(login) {
-  if (login.length < 4 || login.length > 16) {
-    return false;
-  }
-  return true;
+  return login.length >= 4 && login.length <= 16;
 };
 
 const isLoginUnique = function(allLogins, login) {
-  if (allLogins.includes(login) ? true : false) {
-    return false;
-  }
-  return true;
+  return allLogins.includes(login);
 };
 
 const addLogin = function(allLogins, login) {
-  if (isLoginValid(login) === false) {
+  if (!isLoginValid(login)) {
     return "Ошибка! Логин должен быть от 4 до 16 символов";
   }
 
-  if (isLoginUnique(allLogins, login) === false) {
+  if (!isLoginUnique(allLogins, login)) {
     return "Такой логин уже используется!";
   }
 
